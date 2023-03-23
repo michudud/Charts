967646d5fa72539592f696d1215381cf28bcad4b
michudud <pzlc1236@gmail.com>
2023-03-23 22:10:34 +0100

    style update

--- src/components/styles/ChartContainer.styled.js
+++ src/components/styles/ChartContainer.styled.js
@@ -4,7 +4,7 @@ export const ChartContainer = styled.div`
   width: 30vw;
   height: 30vh;
   border: 2px solid #404040;
-  border-radius: 20px;
+  border-radius: 10px;
   margin: 20px;
   background-color: white;
 `;
