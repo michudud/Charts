ba7eafee44863f8f26c00dbbcc54e13e28f69858
michudud <pzlc1236@gmail.com>
2023-05-09 18:08:38 +0200

    style update

--- src/components/styles/ChartContainer.styled.js
+++ src/components/styles/ChartContainer.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ChartContainer = styled.div`
   width: 100%;
   min-height: 300px;
-  border: 2px solid #404040;
+  border: 1px solid #404040;
   border-radius: 10px;
   margin: 20px;
   background-color: white;
--- src/components/styles/FlexColContainer.styled.js
+++ src/components/styles/FlexColContainer.styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const FlexColContainer = styled.div`
   display: flex;
+  overflow: hidden;
   justify-content: center;
   flex-direction: column;
 `;
