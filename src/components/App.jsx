9ce63390e4f9b4d6ef4ebb991a6ed419170c85f3
michudud <pzlc1236@gmail.com>
2023-06-04 23:02:48 +0200

    code cleanup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import OverviewPage from "./OverviewPage";
 import GlobalStyles from "./styles/Global";
--- src/components/OverviewPage.jsx
+++ src/components/OverviewPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import BarChart from "./BarChart";
 import { ChartContainer } from "./styles/ChartContainer.styled";
 import { FlexColContainer } from "./styles/FlexColContainer.styled";
